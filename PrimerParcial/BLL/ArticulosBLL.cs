79aa8cb5e62bbc91c565df2cdcfd699d0b73ec14
johang08 <42912561+johang08@users.noreply.github.com>
2021-02-10 19:38:41 -0400

    Guardar listo

---
+++ PrimerParcial/BLL/ArticulosBLL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerParcial.BLL
+{
+    public class ArticulosBLL
+    {
+
+            public static bool Guardar(Articulos articulo)
+        {
+            if (!Existe(articulo.articuloId))
+                return Insertar(articulo);
+            else
+                return Modificar(articulo);
+        }
+
+    }
+}
